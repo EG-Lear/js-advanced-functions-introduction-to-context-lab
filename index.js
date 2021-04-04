3f6b6d335a7a2f00897d1428391b904e8c2ff5e6
Garret <gwiats@gmail.com>
2021-04-04 16:05:43 -0400

    finished punchIn and punchOut

--- index.js
+++ index.js
@@ -20,16 +20,36 @@ function createEmployeeRecords(employees) { //creates objects for multiple emplo
 
 function createTimeInEvent(employee, punch) { //assigns a time punch to an employee
     let addedPunch = employee
-    addedPunch.timeInEvents = punchIn(punch)
+    addedPunch.timeInEvents.push(punchIn(punch))
     return addedPunch
 }
 
 //2014-02-28 1400
 function punchIn(time) { //takes a string based time punch and returns an object of hour and date
     let seperated = time.split(" ")
+    console.log(seperated)
     const obj = {
         'date': seperated[0],
-        'hour': seperated[1]
+        'hour': parseInt(seperated[1]),
+        'type': "TimeIn"
+    }
+    console.log(obj)
+    return obj
+}
+
+function createTimeOutEvent(employee, punch) { //assings a time out punch
+    let addedPunch = employee
+    addedPunch.timeOutEvents.push(punchOut(punch))
+    return addedPunch
+}
+
+function punchOut(time) { //takes a string based time punch and returns an object of hour and date
+    let seperated = time.split(" ")
+    console.log(seperated)
+    const obj = {
+        'date': seperated[0],
+        'hour': parseInt(seperated[1]),
+        'type': "TimeOut"
     }
     console.log(obj)
     return obj
